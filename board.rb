8eda527de58ff7437b880b3d8d6df1f9d9a8cb39
metacoder87 <gary.miller@gmx.com>
2020-11-17 00:10:46 -0500

    Fixes render and updates tile status

--- board.rb
+++ board.rb
@@ -1,10 +1,10 @@
 
-
+require_relative 'tile'
 
 class Board
 
     def self.the_grid(n)
-        Array.new(n) { Array.new(n) { Tile.new() } }
+        Array.new(n) { Array.new(n) { Tile.new(true) } }
     end
 
     attr_reader :grid
@@ -14,7 +14,7 @@ class Board
     end
 
     def render
-        grid.each { |row| puts row }
+       @grid.dup.each { |line| p line.each_with_index { |tile, i| tile.hidden ? line[i] = "*" : line[i] = "_" } }
     end
 
 end
