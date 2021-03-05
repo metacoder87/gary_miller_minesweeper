2f96b7afa37c5da69ffc66cfdf0fc4ce6a424522
metacoder87 <gary.miller@gmx.com>
2021-03-05 13:52:30 -0500

    Adds in the flag aspect for tile.

--- tile.rb
+++ tile.rb
@@ -3,12 +3,13 @@
 
 class Tile
 
-    attr_accessor :hidden
+    attr_accessor :hidden, :flagged
     attr_reader :bomb
 
     def initialize(status)
         @bomb = bomb?
         @hidden = status
+        @flagged = false
     end
 
     def bomb?
