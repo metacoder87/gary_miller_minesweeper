16c9f2b49c3576f3899e630298dd9b62c31ab32f
metacoder87 <gary.miller@gmx.com>
2021-03-07 10:28:19 -0500

    Adjusts the bomb assignment to the board.

--- tile.rb
+++ tile.rb
@@ -6,14 +6,14 @@ class Tile
     attr_accessor :hidden, :flagged
     attr_reader :bomb
 
-    def initialize
-        @bomb = bomb?
+    def initialize(bomb)
+        @bomb = bomb
         @hidden = true
         @flagged = false
     end
 
-    def bomb?
-        Array.new(7,false).push(true).sample
-    end
+    # def bomb?
+    #     Array.new(7,false).push(true).sample
+    # end
 
 end
