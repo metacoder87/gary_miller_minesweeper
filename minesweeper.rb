d6d96413e44a6aea636dd3fa539a93da1a9decbe
metacoder87 <gary.miller@gmx.com>
2021-03-08 03:17:45 -0500

    Starting a MineSweeper class

--- minesweeper.rb
+++ minesweeper.rb
@@ -3,6 +3,16 @@
 
 class MineSweeper
 
+    def get_size
+        size = nil
+        until size && valid_size?(size)
+            puts "What size board would you like to sweep for mines? (You can say, 3, 6, 9, 12...)"
+            print "> "
+            
+    end
 
+    def get_dif
+
+    end
 
 end
