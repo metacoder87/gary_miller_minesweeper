2910ba5cd63fef77a4a63845b7c9f98e1f4f17a6
metacoder87 <gary.miller@gmx.com>
2021-03-10 21:03:47 -0500

    Adds the get_size method

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,9 +6,13 @@ class MineSweeper
     def initialize
     def get_size
         size = nil
-        until size && valid_size?(size)
+        until size && (1..10).include?(size)
             puts "What size board would you like to sweep for mines? (You can say, 3, 6, 9, 12...)"
             print "> "
+            size = gets.chomp.to_i
+        end
+        size
+    end
             
     end
 
